a52d08b74030f1b6cdccee577e70804723c5e924
tunafishyummy <adinoellamio@gmail.com>
2026-03-10 15:00:53 +0800

    may exam pala

---
+++ BankManagement/Main.java
@@ -0,0 +1,14 @@
+package BankManagement;
+
+import java.util.Scanner;
+
+static String createAccount() {
+
+    
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
